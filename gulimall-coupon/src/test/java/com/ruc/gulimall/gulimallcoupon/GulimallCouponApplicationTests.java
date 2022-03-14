8ad9815959ad3d5d1664f66b007bbcf865cc838e
xiaowenbin <xiaowenbin@ylmf1000.onexmail.com>
2022-03-14 15:05:52 +0800

    feat:project structure

---
+++ gulimall-coupon/src/test/java/com/ruc/gulimall/gulimallcoupon/GulimallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ruc.gulimall.gulimallcoupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ gulimall-ware/src/test/java/com/ruc/gulimall/gulimallware/GulimallWareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ruc.gulimall.gulimallware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallWareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
