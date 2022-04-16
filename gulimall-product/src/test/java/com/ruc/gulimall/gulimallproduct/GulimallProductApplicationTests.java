f0eb4c0342e16a1677b773a857be85af282cfd0d
LAPTOP-H6RCKGQ2\xwb <wenbinxiao1997@gmail.com>
2022-04-17 01:57:51 +0800

    feat：brand crud and file upload by aliyun

--- gulimall-product/src/test/java/com/ruc/gulimall/gulimallproduct/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/com/ruc/gulimall/gulimallproduct/GulimallProductApplicationTests.java
@@ -1,30 +1,39 @@
 package com.ruc.gulimall.gulimallproduct;
 
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
-import com.ruc.gulimall.gulimallproduct.entity.BrandEntity;
-import com.ruc.gulimall.gulimallproduct.service.BrandService;
+import com.aliyun.oss.*;
+//import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+//import com.ruc.gulimall.gulimallproduct.entity.BrandEntity;
+//import com.ruc.gulimall.gulimallproduct.service.BrandService;
+import javafx.application.Application;
 import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+@SpringBootTest()
 class GulimallProductApplicationTests {
 
-    @Autowired
-    BrandService brandService;
+//    @Autowired
+//    BrandService brandService;
 
-    @Test
-    void contextLoads() {
-//        BrandEntity brandEntity = new BrandEntity();
-//        brandEntity.setName("华为");
-//        brandService.save(brandEntity);
-//        BrandEntity brandEntity = new BrandEntity();
-//        brandEntity.setBrandId(1L);
-//        brandEntity.setDescript("华子");
-//        brandService.updateById(brandEntity);
-//        System.out.println("保存成功");
-        BrandEntity brand = brandService.getOne(new QueryWrapper<BrandEntity>().eq("brand_id", 1L));
-        System.out.println(brand);
-    }
+
+//    @Test
+//    void contextLoads() {
+////        BrandEntity brandEntity = new BrandEntity();
+////        brandEntity.setName("华为");
+////        brandService.save(brandEntity);
+////        BrandEntity brandEntity = new BrandEntity();
+////        brandEntity.setBrandId(1L);
+////        brandEntity.setDescript("华子");
+////        brandService.updateById(brandEntity);
+////        System.out.println("保存成功");
+////        BrandEntity brand = brandService.getOne(new QueryWrapper<BrandEntity>().eq("brand_id", 1L));
+////        System.out.println(brand);
+//    }
 
 }
