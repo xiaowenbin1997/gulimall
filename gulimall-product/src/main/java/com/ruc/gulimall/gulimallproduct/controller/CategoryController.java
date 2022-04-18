85e53ec00d225b0e56fe588cc546ddeeca12414d
xiaowenbin <xiaowenbin@ylmf1000.onexmail.com>
2022-04-18 11:37:00 +0800

    feat：update

--- gulimall-product/src/main/java/com/ruc/gulimall/gulimallproduct/controller/CategoryController.java
+++ gulimall-product/src/main/java/com/ruc/gulimall/gulimallproduct/controller/CategoryController.java
@@ -66,6 +66,14 @@ public class CategoryController {
     /**
      * 修改
      */
+    @RequestMapping("/update/sort")
+    public R updateSort(@RequestBody CategoryEntity[] category){
+        categoryService.updateBatchById(Arrays.asList(category));
+        return R.ok();
+    }
+    /**
+     * 修改
+     */
     @RequestMapping("/update")
     public R update(@RequestBody CategoryEntity category){
 		categoryService.updateById(category);
